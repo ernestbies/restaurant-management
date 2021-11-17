37a64502546e60be8a3946e94125a40d8c755cc2
Ernest Bieś <ernestbies@gmail.com>
2021-11-17 14:42:21 +0100

    Mail update.

--- src/components/ContactInfo/ContactInfo.js
+++ src/components/ContactInfo/ContactInfo.js
@@ -5,7 +5,7 @@ import {MAIN_WEBSITE_URL} from "../../helpers/fetchData";
 const ContactInfo = () => {
     return (<ContactInfoContainer>
         <StyledText>{'Check also other projects presented on '}<FormattedText href={MAIN_WEBSITE_URL}>{'my website'}</FormattedText>
-        {' or contact me via e-mail at '}<FormattedText href={'mailto:contact@ernestbies.com'}>{'contact@ernestbies.com'}</FormattedText>{'.'}</StyledText>
+        {' or contact me via e-mail at '}<FormattedText href={'mailto:ernestbies@gmail.com'}>{'ernestbies@gmail.com'}</FormattedText>{'.'}</StyledText>
     </ContactInfoContainer>);
 };
 
